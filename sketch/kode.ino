70069c69ea680da23c6dfad1ab0144825ca0897c
Sembre <agusf8@gmail.com>
2022-05-30 12:19:47 +0700

    Create kode.ino

---
+++ sketch/kode.ino
@@ -0,0 +1,40 @@
+#define waktutunda 10 //untuk 10 detik
+ 
+const int ledPin = 13; //Menggunakan led built in Arduino
+const int SensorPir = 2; //menggunakan pin ke-2 arduino
+ 
+unsigned long now = millis();
+unsigned long TriggerAkhir = 0;
+boolean waktumulai = false;
+ 
+void setup() {
+  Serial.begin(115200); //baut komunikasi serial monitor
+  
+  pinMode(SensorPir, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SensorPir), deteksigerakan, RISING);
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+}
+ 
+void loop() {
+  now = millis();
+  if(waktumulai && (now - TriggerAkhir > (waktutunda*1000))) {
+    Serial.println("Tidak ada gerakan!");
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Air Keluar!");
+    waktumulai = false;
+    delay(10000); 
+    digitalWrite(ledPin, LOW);
+    Serial.println("Air Mati!");
+  }
+}
+ 
+void deteksigerakan() {
+  Serial.println("GERAKAN TERDETEKSI");
+  digitalWrite(ledPin, LOW);
+  waktumulai = true;
+  TriggerAkhir = millis();
+}
+ 
+ 
